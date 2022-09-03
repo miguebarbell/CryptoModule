3dafe413087c82e35120f509c9970b58848d82d2
Miguel R <miguel@debloat.us>
2022-09-02 23:24:47 -0400

    list of currencies

---
+++ server/src/main/java/com/crypto/server/user/UserController.java
@@ -0,0 +1,13 @@
+package com.crypto.server.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+//	@PostMapping("/users")
+//	public String getUser(@RequestParam(String user)) {
+//		return user;
+//	}
+}
