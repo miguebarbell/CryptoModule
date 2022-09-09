f95b0594ad308f0602d6fbb2304f3095ab86e1a1
jerrykankelborg <jkankelborg@csumb.edu>
2022-09-08 17:04:15 -0700

    Dashboard for crypto created

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
+import {BrowserRouter} from "react-router-dom";
+
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+   <React.StrictMode>
+     <DevSupport ComponentPreviews={ComponentPreviews}
+                 useInitialHook={useInitial}>
+       <BrowserRouter>
+         <App/>
+       </BrowserRouter>
+     </DevSupport>
+   </React.StrictMode>);
