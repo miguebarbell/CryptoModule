8cf8fb590d0f1f1ffd6fe1f97126e88847c9b73d
Miguel R <miguel@debloat.us>
2022-09-10 16:00:01 -0400

    chart rendering the correct information

--- client/src/components/chart.jsx
+++ client/src/components/chart.jsx
@@ -25,42 +25,30 @@ ChartJS.register(
 const labels = [];
 const dataset = [];
 const Chart = ({user}) => {
-
-	// const defaultCurrencies = ["ETH", "SOL"];
 	const [cryptoUser, setCryptoUser] = useState(findUserr);
-	// const [cryptoAvailableCurrencies, setcryptoAvailableCurrencies] = useState(defaultCurrencies);
-	// const [cryptoDailyValues, setcryptoDailyValues] = useState(btcDailyData);
-	// const [news, setNews] = useState([]);
-	// const [dataset, setDataset]= useState([])
-	// const [labels, setLabels] = useState([])
 	const [hackyUpdates, setHackyUpdates] = useState(0);
 	useEffect(() => {
 		const fetch = async () => {
 			try {
 				const userPromised = await findUser(user);
 				setCryptoUser(userPromised.data.cryptoUser);
-				// let currenciesArrayString = "[";
 				userPromised.data.cryptoFindUser.currencies.map(async (currency, index) => {
-					// currenciesArrayString += `"${currency}"`;
-					// if (userPromised.data.cryptoFindUser.currencies.length -1 !== index) currenciesArrayString += ", ";
 					const fetchValues = async () => {
-						try {
-							const dailyValuesPromised = await cryptoDaily(currency);
-							// setLabels([])
-							let data = [];
-							if (dailyValuesPromised.data.cryptoDaily) for await (let valuePromised of dailyValuesPromised.data.cryptoDaily) {
-								data.push(valuePromised.value);
-								if (labels.length < dailyValuesPromised.data.cryptoDaily.length) labels.push(valuePromised.date);
-							}
-							// console.log(labels);
-							// setLabels(labels)
-							let chartdata = {
-								label          : currency,
-								data           : data,
-								backgroundColor: colors[index],
+							try {
+								const dailyValuesPromised = await cryptoDaily(currency);
+								let data = [];
+								if (dailyValuesPromised.data.cryptoDaily && (dataset.length <= userPromised.data.cryptoFindUser.currencies.length)) for await (let valuePromised of dailyValuesPromised.data.cryptoDaily) {
+									data.push(valuePromised.value);
+									if (labels.length < dailyValuesPromised.data.cryptoDaily.length) labels.push(valuePromised.date);
+								}
+								let chartdata = {
+									label          : currency,
+									data           : data,
+									backgroundColor: colors[index],
 								borderColor    : 'rgb(53, 62, 235)'
 							};
-							dataset.push(chartdata);
+								if (dataset.map(values => values.label)
+								           .filter(value => value === currency).length === 0) dataset.push(chartdata);
 						} catch (err) {
 							console.log("error fetching daily values");
 							console.log(err);
@@ -68,7 +56,6 @@ const Chart = ({user}) => {
 					};
 					await fetchValues();
 				});
-				// currenciesArrayString += "]";
 			} catch (err) {
 				console.log("error fetching user");
 				console.log(err);
@@ -85,7 +72,6 @@ const Chart = ({user}) => {
 		"rgba(245, 142, 39, 0.5"
 	];
 	const data = {
-		// labels : hardlabels,
 		labels,
 		datasets: dataset
 	};
@@ -104,10 +90,6 @@ const Chart = ({user}) => {
 			}
 		}
 	};
-
-	console.log(data);
-	console.log(labels);
-	console.log(hackyUpdates);
 	return (
 		<>
 			<div>This is the chart</div>
